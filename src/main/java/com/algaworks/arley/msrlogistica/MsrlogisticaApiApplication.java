a84d3e7cc91e0e7e380d0d3e5cc825e65ccb78be
Arley Novais do Nascimento <nascimentoarle@gmail.com>
2022-05-16 21:09:42 -0300

    Add spring starter project

---
+++ src/main/java/com/algaworks/arley/msrlogistica/MsrlogisticaApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.arley.msrlogistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsrlogisticaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsrlogisticaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/arley/msrlogistica/MsrlogisticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.arley.msrlogistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsrlogisticaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
