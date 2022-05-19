40ab57b250997664bf298271a55f3f16d4f4a3e6
Arley Novais do Nascimento <nascimentoarle@gmail.com>
2022-05-19 18:04:26 -0300

    Add addCliente

--- src/main/java/com/algaworks/arley/msrlogistica/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/arley/msrlogistica/api/controller/ClienteController.java
@@ -9,11 +9,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.arley.msrlogistica.domain.model.Cliente;
@@ -45,5 +48,9 @@ public class ClienteController {
 		}		
 	}
 	
-	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionarCliente(@RequestBody Cliente c) {
+		return repositoryC.save(c);
+	}
 }
