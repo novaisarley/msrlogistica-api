9b86b01056f7e01638a2757c0f894c70485ca6a6
Arley Novais do Nascimento <nascimentoarle@gmail.com>
2022-05-19 14:26:11 -0300

    Add findByNome in ClienteRepository

--- src/main/java/com/algaworks/arley/msrlogistica/domain/repository/ClienteRepository.java
+++ src/main/java/com/algaworks/arley/msrlogistica/domain/repository/ClienteRepository.java
@@ -1,9 +1,15 @@
 package com.algaworks.arley.msrlogistica.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.algaworks.arley.msrlogistica.domain.model.Cliente;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+@Repository
 
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
 }
